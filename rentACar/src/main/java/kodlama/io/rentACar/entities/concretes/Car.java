e855a9b080ce4e5b34c6c18cb2979c0f4ea6741c
AlperenTellioglu <57439983+AlperenTellioglu@users.noreply.github.com>
2023-08-31 16:05:20 +0300

    Model and Car classes added

--- rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
+++ rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
@@ -1,34 +1,43 @@
 package kodlama.io.rentACar.entities.concretes;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table(name="brands")
+@Table(name="cars")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Brand {
+public class Car {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
+
+	@Column(name="plate", unique = true)
+	private String plate;
 	
-	@Column(name="name")
-	private String name;
+	@Column(name="dailyPrize")
+	private double dailyPrize;
 	
-	@OneToMany(mappedBy = "brand")
-	private List<Model> models;
+	@Column(name="modelYear")
+	private int modelYear;
+	
+	@Column(name="state")
+	private int state; //1 - Avaible, 2 - Rented, 3 - Maintenance
+	
+	@ManyToOne
+	@JoinColumn(name="model_id")
+	private Model model;
 }
--- rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Model.java
+++ rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Model.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -14,13 +16,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table(name="brands")
+@Table(name="models")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Brand {
+public class Model {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
@@ -29,6 +31,10 @@ public class Brand {
 	@Column(name="name")
 	private String name;
 	
-	@OneToMany(mappedBy = "brand")
-	private List<Model> models;
+	@ManyToOne
+	@JoinColumn(name="brand_id")
+	private Brand brand;
+	
+	@OneToMany(mappedBy = "model")
+	private List<Car> cars;
 }
