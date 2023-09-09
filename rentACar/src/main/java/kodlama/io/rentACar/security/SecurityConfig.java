5569a975acf02b61a1dd3dfb8e47ae549f8f5648
AlperenTellioglu <57439983+AlperenTellioglu@users.noreply.github.com>
2023-09-09 11:16:50 +0300

    Security Config, Login Controller and login ui added

---
+++ rentACar/src/main/java/kodlama/io/rentACar/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package kodlama.io.rentACar.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails alp = User.builder()
+				.username("alp")
+				.password("{noop}5561")
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(alp);
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(configurer ->
+				configurer
+						.anyRequest().authenticated()
+			)
+				.formLogin(form ->
+						form
+								.loginPage("/showMyLoginPage")
+								.loginProcessingUrl("/authenticateTheUser")
+								.permitAll());
+		
+		return http.build();
+	}
+}
